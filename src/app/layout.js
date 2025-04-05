8d48c3bec8de9eeb73b8572f942a86e02d91c23a
zahid-official <zahid.official8@gmail.com>
2025-04-05 23:03:07 +0600

    Dark Mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0b1120] text-white`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-[#0d1424] dark:text-white`}
       >
         {/* header */}
         <nav>
