f9f9fcf36173edf008b45ce473b3cb2a7b787a10
zahid-official <zahid.official8@gmail.com>
2025-04-07 21:06:39 +0600

    learning

--- src/app/components/Learning.jsx
+++ src/app/components/Learning.jsx
@@ -16,8 +16,16 @@ const Learning = () => {
       </div>
 
       {/* content */}
-      <div className="flex flex-col-reverse lg:flex-row gap-20 justify-center items-center container mx-auto px-5 pt-20">
+      <div className="flex flex-col lg:flex-row gap-20 justify-center items-center container mx-auto px-5 pt-20">
         {/* left */}
+        <div className="flex-1">
+          <img
+            src="https://www.hablu-programmer.com/img/img-disendow-web-media.svg"
+            alt=""
+          />
+        </div>
+
+        {/* right */}
         <ul className="flex-1 max-h-[420px] overflow-y-scroll px-4">
           {/* 1 */}
           <li className="relative flex gap-6 pb-14">
@@ -29,10 +37,11 @@ const Learning = () => {
 
             <div>
               <h2 className="text-xl pt-1 font-semibold dark:text-white pb-2.5">
-              Interactive Live & Recording Classes
+                Interactive Live & Recording Classes
               </h2>
               <p>
-              Learn Something New Everyday By Watching Recorded Conceptual Classes And Join Live Classes For Clearing Your Doubts.
+                Learn Something New Everyday By Watching Recorded Conceptual
+                Classes And Join Live Classes For Clearing Your Doubts.
               </p>
             </div>
           </li>
@@ -47,10 +56,12 @@ const Learning = () => {
 
             <div>
               <h2 className="text-xl pt-1 font-semibold dark:text-white pb-2.5">
-              Module Based Study Plan
+                Module Based Study Plan
               </h2>
               <p>
-              There Is No Chance Of Falling Behind, With A Module-Wise Structured Study Plan Packed With Quizzes, Assignments & Practice Home Work For Interview Preparation.
+                There Is No Chance Of Falling Behind, With A Module-Wise
+                Structured Study Plan Packed With Quizzes, Assignments &
+                Practice Home Work For Interview Preparation.
               </p>
             </div>
           </li>
@@ -65,10 +76,11 @@ const Learning = () => {
 
             <div>
               <h2 className="text-xl pt-1 font-semibold dark:text-white pb-2.5">
-              Conceptual Crash Course
+                Conceptual Crash Course
               </h2>
               <p>
-              You Are Not Just Enrolling In One Course. It's A Mission To Learn Lifelong Programming Journey With Hablu Programmer.
+                You Are Not Just Enrolling In One Course. It's A Mission To
+                Learn Lifelong Programming Journey With Hablu Programmer.
               </p>
             </div>
           </li>
@@ -83,10 +95,11 @@ const Learning = () => {
 
             <div>
               <h2 className="text-xl pt-1 font-semibold dark:text-white pb-2.5">
-              Freelancing & Job Placement Guide
+                Freelancing & Job Placement Guide
               </h2>
               <p>
-              Only Serious Course Completers Will Get A Freelancing Guide And Job Placement Support From Zero To Until Success Your Career.
+                Only Serious Course Completers Will Get A Freelancing Guide And
+                Job Placement Support From Zero To Until Success Your Career.
               </p>
             </div>
           </li>
@@ -101,10 +114,11 @@ const Learning = () => {
 
             <div>
               <h2 className="text-xl pt-1 font-semibold dark:text-white pb-2.5">
-              Learning Process Track Record
+                Learning Process Track Record
               </h2>
               <p>
-              Track Your Progress In Real Time To See Your Position On The Leader Board And Get Ahead Of Everyone Else In The Competition.
+                Track Your Progress In Real Time To See Your Position On The
+                Leader Board And Get Ahead Of Everyone Else In The Competition.
               </p>
             </div>
           </li>
@@ -119,18 +133,11 @@ const Learning = () => {
 
             <div>
               <h2 className="text-xl pt-1 font-semibold dark:text-white pb-2.5">
-              Unlimited Support
+                Unlimited Support
               </h2>
             </div>
           </li>
-
-          
         </ul>
-
-        {/* right */}
-        <div className="flex-1">
-            <img src="https://www.hablu-programmer.com/img/img-disendow-web-media.svg" alt="" />
-        </div>
       </div>
     </div>
   );
